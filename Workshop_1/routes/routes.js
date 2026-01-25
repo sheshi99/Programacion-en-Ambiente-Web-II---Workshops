6b6fb3a0c12e5144cffdfd751946a4bdbe5cd818
Seidy <seidyalanis@gmail.com>
2026-01-24 18:23:00 -0600

    Se agrega método para eliminar al obtener el ID y el uso de Model.findByIdAndDelete.

--- Workshop_1/routes/routes.js
+++ Workshop_1/routes/routes.js
@@ -82,9 +82,21 @@ router.patch('/update/:id', async (req, res) => {
     }
 })
 
-//Delete by ID Method
+/*Delete by ID Method
 router.delete('/delete/:id', (req, res) => {
     res.send('Delete by ID API')
+})*/
+
+//Delete by ID Method
+router.delete('/delete/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const data = await Model.findByIdAndDelete(id)
+        res.send(`Document with ${data.name} has been deleted..`)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
 })
 
 
