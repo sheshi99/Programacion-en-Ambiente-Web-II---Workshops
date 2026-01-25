a0004bc85ee21f5ff14819c43d6475ba30e38c0a
Seidy <seidyalanis@gmail.com>
2026-01-24 18:13:12 -0600

    Se agrega método para actualizar y eliminar datos según ID.

--- Workshop_1/routes/routes.js
+++ Workshop_1/routes/routes.js
@@ -58,9 +58,28 @@ router.get('/getOne/:id', async (req, res) => {
     }
 })
 
-//Update by ID Method
+/*Update by ID Method
 router.patch('/update/:id', (req, res) => {
     res.send('Update by ID API')
+})*/
+
+
+//Update by ID Method
+router.patch('/update/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const updatedData = req.body;
+        const options = { new: true };
+
+        const result = await Model.findByIdAndUpdate(
+            id, updatedData, options
+        )
+
+        res.send(result)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
 })
 
 //Delete by ID Method
